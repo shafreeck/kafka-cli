800042bae5f93ef40875037f2eba15785003dbd7
Shafreeck Sea <shafreeck@gmail.com>
2017-12-06 04:53:21 +0800

    Implement produce command

--- cmd/produce.go
+++ cmd/produce.go
@@ -15,19 +15,65 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
+	"github.com/Shopify/sarama"
 	"github.com/spf13/cobra"
 )
 
 // produceCmd represents the produce command
 var produceCmd = &cobra.Command{
 	Use:   "produce",
-	Short: "TODO",
+	Short: "produce message",
 	Run: func(cmd *cobra.Command, args []string) {
-		// TODO: Work your own magic here
-		fmt.Println("produce called")
+		if len(args) == 0 {
+			fmt.Println("topic is required")
+			os.Exit(-1)
+		}
+
+		topic := args[0]
+
+		exitOnError := func(err error) {
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(-1)
+			}
+		}
+		producer, err := sarama.NewAsyncProducerFromClient(c)
+		exitOnError(err)
+
+		ch := make(chan []byte)
+		defer producer.Close()
+		go func() {
+			if err := readMessages(os.Stdin, ch); err != nil {
+				exitOnError(err)
+			}
+		}()
+		for msg := range ch {
+			select {
+			case producer.Input() <- &sarama.ProducerMessage{Topic: topic, Value: sarama.ByteEncoder(msg)}:
+			case ack := <-producer.Successes():
+				fmt.Println(ack)
+			case err := <-producer.Errors():
+				exitOnError(err)
+			}
+		}
+
 	},
+}
+
+func readMessages(f *os.File, ch chan []byte) error {
+	reader := bufio.NewReader(f)
+	for {
+		line, err := reader.ReadBytes('\n')
+		if err != nil {
+			return err
+		}
+		ch <- line[0 : len(line)-1]
+	}
+	return nil
 }
 
 func init() {
